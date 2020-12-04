79a293eb6bfec8f128ecad0b483837a4d9de94be
Anwesh43 <anweshthecool0@gmail.com>
2020-12-05 02:21:29 +0530

    adding constants and libraries

--- partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
+++ partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
@@ -1,2 +1,28 @@
 package com.example.partialcirclerectview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#03A9F4",
+    "#009688",
+    "#3F51B5",
+    ""
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rFactor : Float = 8.9f
+val barFactor : Float = 14.9f

d56e72e462fc95097112040d4612bf51625cf949
Anwesh43 <anweshthecool0@gmail.com>
2020-12-05 02:42:21 +0530

    adding Canvas extension methods

--- partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
+++ partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
@@ -31,3 +31,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawPartialCircleRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val bar : Float = Math.min(w, h) / barFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val rSize : Float = size * sf1
+    save()
+    translate(w / 2, h / 2)
+    drawRect(RectF(-rSize, -rSize, rSize, rSize), paint)
+    drawCircle(0f, -(h / 2 + r) * (1f - sf3), r * sf2, paint)
+    drawRect(RectF(-size, -size - bar, -size, -size + bar * sf4), paint)
+    restore()
+}
+
+fun Canvas.drawPCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawPartialCircleRect(scale, w, h, paint)
+}

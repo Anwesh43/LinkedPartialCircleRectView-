4d45c694e54d37b0032baaa3c796588ee7fc6f26
Anwesh43 <anweshthecool0@gmail.com>
2020-12-05 02:45:54 +0530

    adding PartialCircleRectView class

--- partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
+++ partialcirclerectview/src/main/java/com/example/partialcirclerectview/PartialCircleRectView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawPCRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawPartialCircleRect(scale, w, h, paint)
 }
+
+class PartialCircleRectView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
